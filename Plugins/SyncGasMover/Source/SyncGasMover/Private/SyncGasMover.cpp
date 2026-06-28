563f43ef62d1723fa5e813f2a51ee8ca16715f35
Lolor92 <89623320+Lolor92@users.noreply.github.com>
2026-06-28 06:37:56 -0700

    Add blend debug log category

--- Plugins/SyncGasMover/Source/SyncGasMover/Private/SyncGasMover.cpp
+++ Plugins/SyncGasMover/Source/SyncGasMover/Private/SyncGasMover.cpp
@@ -1,22 +1,19 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "SyncGasMover.h"
+#include "Debug/SGM_DebugLog.h"
 
 #define LOCTEXT_NAMESPACE "FSyncGasMoverModule"
 
 DEFINE_LOG_CATEGORY(LogSyncGasMover);
+DEFINE_LOG_CATEGORY(LogSyncGasMoverBlend);
 
 void FSyncGasMoverModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 }
 
 void FSyncGasMoverModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
 }
 
 #undef LOCTEXT_NAMESPACE
-	
+
 IMPLEMENT_MODULE(FSyncGasMoverModule, SyncGasMover)
